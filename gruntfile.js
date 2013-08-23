771fdc1a64b302f5d508e11012c84bc0b1445711
Prathamesh Satpute <prathamesh.satpute@gmail.com>
2013-08-22 23:58:52 -0400

    Updated structure to use with angular and coral

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function (grunt) {
+0module.exports = function (grunt) {
 
 	grunt.initConfig({
 
@@ -14,19 +14,12 @@ module.exports = function (grunt) {
 		copy: {
 			jslibs: {
 				files: {
-					'src/public/js/lib/jquery.js': 'components/jquery/jquery.js',
 					'src/public/js/lib/underscore.js': 'components/underscore/underscore.js',
-					'src/public/js/lib/backbone.js': 'components/backbone/backbone.js',
-					'src/public/js/lib/handlebars.runtime.js': 'components/handlebars/handlebars.runtime.js',
 					'src/public/js/lib/require.js': 'components/requirejs/require.js'
 				}
 			},
 			csslibs: {
 				files: {
-					'src/public/css/bootstrap.css': 'components/bootstrap/docs/assets/css/bootstrap.css',
-					'src/public/css/bootstrap-responsive.css': 'components/bootstrap/docs/assets/css/bootstrap-responsive.css'
-					'src/public/img/glyphicons-halflings-white.png': 'components/bootstrap/img/glyphicons-halflings-white.png'
-					'src/public/img/glyphicons-halflings.png': 'components/bootstrap/img/glyphicons-halflings.png'
 				}
 			},
 			server: {
